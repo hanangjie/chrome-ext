3ffac6469e62cfa67f3b8d5c4cb210735381723d
韩志万 <han.zhw@ziytek.cn>
2025-04-24 22:31:41 +0800

    ff

--- pan/start.bat
+++ pan/start.bat
@@ -1,6 +1,6 @@
 @echo off
 set NODE_SCRIPT="g:/hzw/chrome-ext/pan/index.js"
-set GIT_BASH_PATH="C:\Program Files\Git\git-bash.exe"
+set GIT_BASH_PATH="F:\Git\git-bash.exe"
 
 for /L %%i in (1,1,10) do (
   start "" %GIT_BASH_PATH% -c "node %NODE_SCRIPT% --worker=%%i"
