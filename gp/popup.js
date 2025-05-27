6f627bed03cf13a8760eb6f6c81fea132cb86054
韩志万 <han.zhw@ziytek.cn>
2025-05-27 13:58:13 +0800

    ff

--- gp/popup.js
+++ gp/popup.js
@@ -10,9 +10,9 @@ function createEventSource() {
     }
   });
   eventSource = new EventSource(
-    "https://1.push2.eastmoney.com/api/qt/ulist/sse?invt=3&pi=0&pz=100&mpi=2000&secids=" +
-      sseId.join(",") +
-      "&ut=6d2ffaa6a585d612eda28417681d58fb&fields=f12,f13,f19,f14,f139,f148,f2,f4,f1,f125,f18,f3,f152,f5,f30,f31,f32,f6,f8,f7,f10,f22,f9,f112,f100,f62&po=1"
+    "https://1.push2.eastmoney.com/api/qt/ulist/sse?invt=3&pi=0&pz=100&mpi=10&secids=" +
+    sseId.join(",") +
+    "&ut=6d2ffaa6a585d612eda28417681d58fb&fields=f12,f13,f14,f19,f139,f148,f2,f4,f1,f125,f18,f3,f152,f6,f8,f7,f10,f15,f16,f20,f21&po=1"
   );
   eventSource.addEventListener("message", function (e) {
     const result = JSON.parse(e.data);
@@ -45,25 +45,25 @@ function createEventSource() {
       const valChange = item.f4 / 100;
       total = total + +(gpList[item.f12].gu || 0) * valChange;
       const textCode = `${item.f13 === 0 ? "sz" : "sh"}${item.f12}`;
-      html += `<li style="color:${
-        zf > 0 ? "red" : "green"
-      }"><a style="text-decoration: none;color:${
-        zf > 0 ? "red" : "green"
-      }" href="https://quote.eastmoney.com/${textCode}.html#fullScreenChart" target="_blank">${
-        item.f12
-      } ${item.f14} ${(item.f2 / 100).toFixed(2)} ${zf.toFixed(2)}%</a>
-      <span style="float:right;margin-left:5px" data-id="${
-        item.f12
-      }" >删除 </span>
-      <a style="float:right;margin-left:5px" href="https://quote.eastmoney.com/f1.html?newcode=${
-        item.f13
-      }.${item.f12}"  target="_blank">分时 </a>
-      <a style="float:right;margin-left:5px" href="https://data.eastmoney.com/zjlx/${
-        item.f12
-      }.html#chart-k-cyq"  target="_blank">资金 </a>
-      <a style="float:right;margin-left:5px" href="https://emweb.securities.eastmoney.com/pc_hsf10/pages/index.html?type=web&code=${
-        item.f13 === 0 ? "SZ" : "SH"
-      }${item.f12}&color=b#/gdyj"  target="_blank">股东</a>
+      html += `<li style="color:${zf > 0 ? "red" : "green"
+        }" title="
+        最高:${(item.f15 / 100).toFixed(2)}
+        最低:${(item.f16 / 100).toFixed(2)}
+        昨收：${(item.f18 / 100).toFixed(2)}
+        量比：${(item.f10 / 100).toFixed(2)}
+        总市:${(item.f20 / 10000 / 10000).toFixed(2)}亿
+        流市:${(item.f21 / 10000 / 10000).toFixed(2)}亿
+        "><a style="text-decoration: none;color:${zf > 0 ? "red" : "green"
+        }" href="https://quote.eastmoney.com/${textCode}.html#fullScreenChart" target="_blank">${item.f12
+        } ${item.f14} ${(item.f2 / 100).toFixed(2)} ${zf.toFixed(2)}%</a>
+      <span style="float:right;margin-left:5px" data-id="${item.f12
+        }" >删除 </span>
+      <a style="float:right;margin-left:5px" href="https://quote.eastmoney.com/f1.html?newcode=${item.f13
+        }.${item.f12}"  target="_blank">分时 </a>
+      <a style="float:right;margin-left:5px" href="https://data.eastmoney.com/zjlx/${item.f12
+        }.html#chart-k-cyq"  target="_blank">资金 </a>
+      <a style="float:right;margin-left:5px" href="https://emweb.securities.eastmoney.com/pc_hsf10/pages/index.html?type=web&code=${item.f13 === 0 ? "SZ" : "SH"
+        }${item.f12}&color=b#/gdyj"  target="_blank">股东</a>
       &nbsp;换 ${(item.f8 / 100).toFixed(2)}%</li>`;
     });
     // $("#list").html(html + `<li>盈亏：${total}</li>`);
